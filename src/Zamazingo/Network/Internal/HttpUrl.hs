dcb32a2cae951f62941589940d254bd22e54866b
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2022-01-17 17:15:53 +0800

    feat: add Aeson instances to HttpUrl, revisit documentation

--- src/Zamazingo/Network/Internal/HttpUrl.hs
+++ src/Zamazingo/Network/Internal/HttpUrl.hs
@@ -7,11 +7,18 @@ module Zamazingo.Network.Internal.HttpUrl where
 
 import           Control.Monad                     (when)
 import           Control.Monad.Except              (MonadError(throwError))
+import qualified Data.Aeson                        as Aeson
 import           Data.Text                         (Text)
 import qualified Language.Haskell.TH.Syntax        as TH.Syntax
 import qualified Text.URI                          as Uri
 import qualified Text.URI.QQ                       as Uri.QQ
-import           Zamazingo.Text.Internal.TextCodec (TextCodec, TextDecoder(..), TextEncoder(..))
+import           Zamazingo.Text.Internal.TextCodec
+                 ( TextCodec
+                 , TextDecoder(..)
+                 , TextEncoder(..)
+                 , jsonDecoderFromTextDecoder
+                 , jsonEncoderFromTextEncoder
+                 )
 
 
 -- | Type encoding of an HTTP URI.
@@ -66,6 +73,29 @@ instance TextEncoder HttpUrl where
   encodeText = Uri.render . getHttpUri
 
 
+-- | 'Aeson.FromJSON' instance for 'HttpUrl'.
+--
+-- >>> Aeson.eitherDecode "\"https://local.host\"" :: Either String HttpUrl
+-- Right URI {uriScheme = Just "https", uriAuthority = Right (Authority {authUserInfo = Nothing, authHost = "local.host", authPort = Nothing}), uriPath = Nothing, uriQuery = [], uriFragment = Nothing}
+-- >>> Aeson.eitherDecode "\"https://\"" :: Either String HttpUrl
+-- Left "Error in $: Not a valid HTTP(S) URI in our concept: https://"
+-- >>> Aeson.eitherDecode "\"smtp://localhost\"" :: Either String HttpUrl
+-- Left "Error in $: Not a valid HTTP(S) URI in our concept: smtp://localhost"
+instance Aeson.FromJSON HttpUrl where
+  parseJSON = jsonDecoderFromTextDecoder "HttpUrl"
+
+
+-- | 'Aeson.ToJSON' instance for 'HttpUrl'.
+--
+-- >>> :set -XTemplateHaskell
+-- >>> import qualified Zamazingo as Z
+-- >>> Aeson.encode ($$(Z.decodeTextTH "https://local.host") :: HttpUrl)
+-- "\"https://local.host\""
+instance Aeson.ToJSON HttpUrl where
+  toJSON = jsonEncoderFromTextEncoder
+
+
+-- | Smart constructor for 'HttpUrl' values.
 mkHttpUrl :: MonadError Text m => Uri.URI -> m HttpUrl
 mkHttpUrl u = case Uri.uriScheme u of
   Nothing -> err
@@ -76,9 +106,11 @@ mkHttpUrl u = case Uri.uriScheme u of
     err = throwError ("URI is not an HTTP(S) URI: " <> Uri.render u)
 
 
+-- | HTTP URI scheme.
 uriSchemeHttp :: Uri.RText l
 uriSchemeHttp = [Uri.QQ.scheme|http|]
 
 
+-- | HTTPS URI scheme.
 uriSchemeHttps :: Uri.RText l
 uriSchemeHttps = [Uri.QQ.scheme|https|]
